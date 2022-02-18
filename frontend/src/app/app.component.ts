6c9cad783821419de5d8b618b3f16ea445d573de
admin <admin@admin.com>
2022-02-18 09:33:45 +0600

    Create a link shortener component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,8 +1,45 @@
 import { Component } from '@angular/core';
+import { LinkService } from './shared/link.service';
+import { Link } from './shared/link.model';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  link: Link | null = null;
+
+  originalUrl = '';
+  isLinkRequested = false;
+
+  apiUrl = environment.apiUrl;
+
+  constructor(
+    private linkService: LinkService,
+  ) {}
+
+  onSubmit() {
+    if (!this.isValidForm()) {
+      return;
+    }
+
+    this.isLinkRequested = true;
+    this.linkService.getLink(this.originalUrl)
+      .subscribe({
+        next: (link) => {
+          this.link = link;
+          this.isLinkRequested = false;
+        },
+        error: () => {
+          this.isLinkRequested = false;
+        }
+      });
+  }
+
+  isValidForm() {
+    return this.originalUrl && !this.isLinkRequested;
+  }
+
+}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { LoaderComponent } from './ui/loader/loader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
