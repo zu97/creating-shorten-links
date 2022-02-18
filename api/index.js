5002c9ecb80081c67915d03c2555a19463478b4e
admin <admin@admin.com>
2022-02-18 08:50:27 +0600

    Creating a database connection

--- api/index.js
+++ api/index.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const config = require('./config');
 
 const app = express();
 const port = 8000;
 
-app.listen(port, () => {
-    console.log(`Server started on ${port} port`);
-});
+app.use(cors());
+app.use(express.json());
+
+const run = async () => {
+    await mongoose.connect(config.mongo.db, config.mongo.options);
+
+    app.listen(port, () => {
+        console.log(`Server started on ${port} port`);
+    });
+
+    process.on('exit', () => {
+        mongoose.disconnect();
+    });
+}
+
+run().catch(e => console.log(e));
 
 
 
